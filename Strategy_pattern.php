6e3a59c2365f4405db4b9b2781eb3598ba37cbe9
Michail <futurer@inbox.ru>
2017-05-22 21:36:40 +0400

    Пример использования шаблона "Стратегия"

---
+++ Strategy_pattern.php
@@ -0,0 +1,65 @@
+abstract class Lesson {
+    private   $duration;
+    private   $costStrategy;
+
+    function __construct( $duration, CostStrategy $strategy ) {
+        $this->duration = $duration;
+        $this->costStrategy = $strategy;
+    }
+
+    function cost() {
+        return $this->costStrategy->cost( $this );
+    }
+
+    function chargeType() {
+        return $this->costStrategy->chargeType( );
+    }
+
+    function getDuration() {
+        return $this->duration;
+    }
+
+    //какие-то ещё методы класса...
+}
+
+
+abstract class CostStrategy {
+    abstract function cost( Lesson $lesson );
+    abstract function chargeType();
+}
+
+class TimedCostStrategy extends CostStrategy {
+    function cost( Lesson $lesson ) {
+        return ( $lesson->getDuration() * 5 );
+    }
+    function chargeType() {
+        return "почасовая оплата";
+    }
+}
+
+class FixedCostStrategy extends CostStrategy {
+    function cost( Lesson $lesson ) {
+        return 30;
+    }
+
+    function chargeType() {
+        return "фиксированная оплата";
+    }
+}
+
+class Lecture extends Lesson {
+    //Уточнение специфичной реализации класса Лекция
+}
+
+class Seminar extends Lesson {
+    //Уточнение специфичной реализации класса Семинар
+}
+
+//заполняем массив новыми объектами
+$lessons[] = new Seminar( 4, new TimedCostStrategy() );
+$lessons[] = new Lecture( 4, new FixedCostStrategy() );
+
+foreach ( $lessons as $lesson ) { //выводим содержимое объектов, содержащихся в массиве
+    print "lesson charge {$lesson->cost()}. ";
+    print "Charge type: {$lesson->chargeType()}\n";
+}
