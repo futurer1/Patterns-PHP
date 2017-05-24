5eac26b23f6ce6335537debcdf4c7a51a3f05a5f
Michail <futurer@inbox.ru>
2017-05-24 23:34:08 +0400

    Update Factory_method_pattern.php

--- Factory_method_pattern.php
+++ Factory_method_pattern.php
@@ -1,1 +1,13 @@
+/**
+* Шаблон Factory Method
+*/
 
+abstract class ApptEncoder {
+    abstract function encode();
+}
+
+class BloggsApptEncoder extends ApptEncoder {
+    function encode() {
+        return "Кодируем данные в формат Bloggs<br />";
+    }
+}
