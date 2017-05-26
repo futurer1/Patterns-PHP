a5bd17dbf61303faa6da1e43920eb83f0218b2c4
Michail <futurer@inbox.ru>
2017-05-26 18:24:04 +0400

    Update Prototype_pattern.php

--- Prototype_pattern.php
+++ Prototype_pattern.php
@@ -3,3 +3,15 @@
 * Шаблон Prototype
 * Особенность заключается в том, что происходит клонирование уже созданных объектов с сохранением их первоначального статуса
 */
+abstract class Sea {}    //супер-класс 1
+public class EarthSea extends Sea {}    //реализация 1
+public class MarsSea extends Sea {}    //реализация 2
+
+abstract class Plains {}    //супер-класс 2
+public class EarthPlains extends Plains {}
+public class MarsPlains extends Plains {}
+
+abstract class Forest {}    //супер-класс 3
+public class EarthForest extends Forest {}
+public class MarsForest extends Forest {}
+
