b706435725e744eb48ca06ca759df0c321801dfc
Michail <futurer@inbox.ru>
2017-05-26 18:28:18 +0400

    Update Prototype_pattern.php

--- Prototype_pattern.php
+++ Prototype_pattern.php
@@ -15,3 +15,26 @@ abstract class Forest {}    //супер-класс 3
 public class EarthForest extends Forest {}
 public class MarsForest extends Forest {}
 
+class TerrainFactory {
+    private $sea;    //переменная private для хранения объекта-образца одной из реализаций типа Sea
+    private $plains;    //переменная private для хранения объекта-образца одной из реализаций типа Plains
+    private $forest;    //переменная private для хранения объекта-образца одной из реализаций типа Forest
+
+    function __construct( Sea $sea, Plains $plains, Forest $forest ) {    //конструктор заносит объекты в переменные класса
+        $this->sea = $sea;
+        $this->plains = $plains;
+        $this->forest = $forest;
+    }
+
+    function getSea() {
+        return clone $this->sea;    //возвращаем клонированный объект из уже созданного, т.е. из прототипа
+    }
+
+    function getPlains() {
+        return clone $this->plains;
+    }
+
+    function getForest() {
+        return clone $this->forest;
+    }
+}
