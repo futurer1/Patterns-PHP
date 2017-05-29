e24a6f1708b7341bd4cc136de1344e1af7858468
Michail <futurer@inbox.ru>
2017-05-29 19:03:01 +0400

    Update Composite_pattern.php

--- Composite_pattern.php
+++ Composite_pattern.php
@@ -1,3 +1,29 @@
+<?php
 /**
 * Шаблон Composite
 */
+
+abstract class Unit {
+    abstract function addUnit(Unit $unit);    //обязательный метод для добавления объектов
+    abstract function removeUnit(Unit $unit); //обязательный метод для удаления объектов
+    abstract function bombardStrength();    //любой пользовательский метод
+}
+
+class Army extends Unit {
+    private $units = array();    //массив для хранения объектов класса Unit
+    
+    public function addUnit(Unit $unit)
+    {
+        // TODO: Implement addUnit() method.
+    }
+    
+    public function removeUnit(Unit $unit)
+    {
+        // TODO: Implement removeUnit() method.
+    }
+    
+    public function bombardStrength()
+    {
+        // TODO: Implement bombardStrength() method.
+    }
+}
