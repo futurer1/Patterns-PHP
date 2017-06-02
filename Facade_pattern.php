c6a5f0114340b230ffa405d4f3c8971ee8144896
Michail <futurer@inbox.ru>
2017-06-02 23:49:12 +0400

    Update Facade_pattern.php

--- Facade_pattern.php
+++ Facade_pattern.php
@@ -2,3 +2,22 @@
 /**
 * Шаблон Facade
 */
+function getProductFileLines( $file )
+{
+    return file( $file );
+}
+
+function getProductObjectFromId( $id, $productname )
+{
+    return new Product( $id, $productname );
+}
+
+class Product {
+    public $id;
+    public $name;
+    function __construct( $id, $name )
+    {
+        $this->id = $id;
+        $this->name = $name;
+    }
+}
