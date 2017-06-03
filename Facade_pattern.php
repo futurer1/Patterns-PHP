1dd3209391d6bf83908c27cd1a203add3d6f6c13
Michail <futurer@inbox.ru>
2017-06-03 15:17:38 +0400

    Update Facade_pattern.php

--- Facade_pattern.php
+++ Facade_pattern.php
@@ -21,3 +21,33 @@ class Product {
         $this->name = $name;
     }
 }
+
+class ProductFacade {
+    private $products = array();
+    private $file;
+    
+    function __construct( $file ) {
+        $this->file = $file;
+        $this->compile();
+    }
+
+    private function compile() {
+        $lines = getProductFileLines( $this->file );
+        foreach ( $lines as $line ) {
+            $id = getIDFromLine( $line );
+            $name = getNameFromLine( $line );
+            $this->products[$id] = getProductObjectFromID( $id, $name  );
+        }
+    }
+
+    function getProducts() {
+        return $this->products;
+    }
+
+    function getProduct( $id ) {
+        if ( isset( $this->products[$id] ) ) {
+            return $this->products[$id];
+        }
+        return null;
+    }
+}
